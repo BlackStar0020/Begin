7cb32cd4cce5ee58569f871b6472c2b84bb34916
dilya <dildoranasriddinzoda07@gmail.com>
2021-08-11 01:34:40 +0500

    first commit

---
+++ begin_abramyan/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace begin_abramyan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            /* int a = int.Parse(Console.ReadLine());
+             int P = 4 * a;
+             Console.WriteLine($"{P}");
+             Console.ReadKey();
+
+         //2
+             int a = int.Parse(Console.ReadLine());
+             double S = Math.Pow(a, 2);
+             Console.WriteLine($" {S}");
+             Console.ReadKey();
+
+         //3
+             int a = int.Parse(Console.ReadLine());
+             int b = int.Parse(Console.ReadLine());
+             int S = a * b;
+             int P = 2 * (a + b);
+             Console.WriteLine($"{S }");
+             Console.WriteLine($"{P}");
+             Console.ReadKey();
+
+         //4
+              double d = int.Parse(Console.ReadLine());
+              double pi = 3.14;
+              double L = pi * d;
+              Console.WriteLine($"{L}");
+              Console.ReadKey();
+
+         //5
+               double a = double.Parse(Console.ReadLine());
+               double V = Math.Pow(a, 2), S=6*(Math.Pow(a, 2));
+               Console.WriteLine(V);
+               Console.WriteLine(S);
+               Console.ReadKey();
+
+               //6
+               double a = double.Parse(Console.ReadLine());
+               double b = double.Parse(Console.ReadLine());
+               double c = double.Parse(Console.ReadLine());
+               double V = a * b * c, S = (a*b+b*c+a*c);
+               Console.WriteLine(V);
+               Console.WriteLine(S);
+               Console.ReadKey();
+
+                //7
+                double R = double.Parse(Console.ReadLine());
+                double pi = 3.14;
+                double L = 2 * pi * R, S = pi * Math.Pow(R, 2);
+                Console.WriteLine(L);
+                Console.WriteLine(S);
+                Console.ReadKey();
+
+                 //8
+                 double a = double.Parse(Console.ReadLine());
+                 double b = double.Parse(Console.ReadLine());
+                 double sred = (a + b) / 2;
+                 Console.WriteLine(sred);
+                 Console.ReadKey();
+
+                 //9
+                 byte a = byte.Parse(Console.ReadLine());
+                 byte b = byte.Parse(Console.ReadLine());
+                 double sredgeo = Math.Sqrt(a*b);
+                 Console.WriteLine(sredgeo);
+                 Console.ReadKey();
+
+                 //10
+
+
+                 //11
+
+                  //12
+                  double a = double.Parse(Console.ReadLine());
+                  double b = double.Parse(Console.ReadLine());
+
+                  double c = Math.Sqrt((Math.Pow(a, 2))) + (Math.Pow(b, 2)),
+                    P = a + b + c;
+                  Console.WriteLine($"{c }, {P }");
+                  Console.ReadKey();
+
+                   //13
+         double pi=3.14;
+                   double R1 = double.Parse(Console.ReadLine());
+                   double R2 = double.Parse(Console.ReadLine());
+
+                   double S1 = pi * Math.Pow(R1, 2), S2 = pi * Math.Pow(R2, 2), S3 = (S1 - S2);
+                   Console.WriteLine($"{S1}, {S2}, {S3}");
+                   Console.ReadKey();
+
+              //14
+              double pi = 3.14;
+              double L = double.Parse(Console.ReadLine());
+
+              double R = L / (2 * pi), S = pi * Math.Pow(R, 2);
+              Console.WriteLine($"{R}, {S}");
+              Console.ReadKey();
+
+
+
+
+             //15
+             double S = double.Parse(Console.ReadLine());
+             double pi = 3.14;
+             double D = Math.Sqrt(4 * S / pi), L = pi * D;
+             Console.WriteLine($"{D} {L}");
+             Console.ReadKey();*/
+
+
+
+
+            //16
+            /* double x1 = double.Parse(Console.ReadLine());
+             double x2 = double.Parse(Console.ReadLine());
+             double restoyanie = Math.Abs(x2-x1);
+             Console.WriteLine($"{restoyanie}");
+             Console.ReadKey();*/
+
+            //17
+
+            /*double A = double.Parse(Console.ReadLine());
+            double B = double.Parse(Console.ReadLine());
+            double C = double.Parse(Console.ReadLine());
+            double AC = C - A, BC = C - B;
+            Console.WriteLine($"AC={AC} BC={BC}");
+            Console.ReadKey();*/
+
+            //18
+            /*
+            double A = double.Parse(Console.ReadLine());
+            double C = double.Parse(Console.ReadLine());
+            double B = double.Parse(Console.ReadLine());
+            double AC = C - A, BC = B - C, Dlina=AC*BC;
+            Console.WriteLine($"{AC}  {BC}  {Dlina}");
+            Console.ReadKey();*/
+
+            //19
+
+
+        }   
+    }
+}
